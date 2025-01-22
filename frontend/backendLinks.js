d9575c29edd5c408cfbb23aa4c8103edbbecf06c
Ajaymali2004 <ajays95372@gmail.com>
2025-01-23 02:17:13 +0530

    backendLink changes

--- frontend/backendLinks.js
+++ frontend/backendLinks.js
@@ -1,5 +1,5 @@
-const WS_URL = "http://localhost:8080/";
-const Auth_URL="http://localhost:5000/";
+const WS_URL = "https://mychess-websocket.onrender.com/";
+const Auth_URL = "https://mychess-anba.onrender.com/";
 const INIT_GAME = "init_game";
 const MOVE = "move";
 const GAME_OVER = "game_over";
@@ -9,5 +9,18 @@ const WAITING_FOR_FRIEND = "waiting_for_friend";
 const CREAT_CHALLENGE = "create_challenge";
 const CANCEL_CHALLENGE = "canceled_the_challenge";
 const ACCEPT_CHALLENGE = "accept_challenge";
-const TIMER_UPDATE="timer_update";
-export {WS_URL,TIMER_UPDATE,Auth_URL,CREAT_CHALLENGE,INIT_GAME,GAME_OVER,PLAY_AGAIN_REQ,PLAY_AGAIN_RES,MOVE,WAITING_FOR_FRIEND,CANCEL_CHALLENGE,ACCEPT_CHALLENGE};
+const TIMER_UPDATE = "timer_update";
+export {
+  WS_URL,
+  TIMER_UPDATE,
+  Auth_URL,
+  CREAT_CHALLENGE,
+  INIT_GAME,
+  GAME_OVER,
+  PLAY_AGAIN_REQ,
+  PLAY_AGAIN_RES,
+  MOVE,
+  WAITING_FOR_FRIEND,
+  CANCEL_CHALLENGE,
+  ACCEPT_CHALLENGE,
+};
--- frontend/src/components/MyVerticallyCenteredModal.jsx
+++ frontend/src/components/MyVerticallyCenteredModal.jsx
@@ -185,7 +185,6 @@ function MyVerticallyCenteredModal({ show, onHide, setLogin }) {
               required
               className="form-control"
               placeholder="Email address"
-              autoComplete="off"
               onChange={onchange}
             />
             <MdAlternateEmail className="fs-5 m-2" />
